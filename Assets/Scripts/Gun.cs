b5ea123e6bc8f6b767573be02cd649590943b2c6
Abdul-KMC <75877950+Abdul-KMC@users.noreply.github.com>
2024-02-28 17:08:05 +0530

    fixed shooting issue after switching

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,7 +1,6 @@
-using System.Collections;
 using UnityEngine.InputSystem;
 using UnityEngine;
-using UnityEngine.InputSystem.Controls;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
@@ -17,22 +16,20 @@ public class Gun : MonoBehaviour
     public GameObject impactEffect;
 
     public int currentAmmo;
-    public int maxAmmo = 100;
-    public int magazineAmmo = 1000;
+    public int maxAmmo = 10;
+    public int magazineAmmo = 30;
 
     public float reloadTime = 2f;
     public bool isReloading;
 
-    //public Animator animator;
+    InputAction shoot;
 
-    private InputAction shoot;
-
-    private void Awake()
+    void Start()
     {
         shoot = new InputAction("Shoot", binding: "<mouse>/leftButton");
-        shoot.AddBinding("<Gamepad>/buttonSouth");
+        shoot.AddBinding("<Gamepad>/x");
 
-        shoot.performed += Shoot;
+        shoot.performed += _ => Fire();  // Attach Fire() method to the performed event
         shoot.Enable();
 
         currentAmmo = maxAmmo;
@@ -41,42 +38,25 @@ public class Gun : MonoBehaviour
     private void OnEnable()
     {
         isReloading = false;
-        //animator.SetBool("isReloading", false);
     }
 
-    private void OnDisable()
-    {
-        shoot.performed -= Shoot;
-        shoot.Disable();
-    }
-
-    private void Shoot(InputAction.CallbackContext context)
+    void Update()
     {
         if (currentAmmo == 0 && magazineAmmo == 0)
         {
-            //animator.SetBool("isShooting", false);
             return;
         }
 
-        if (isReloading)
-            return;
-
-        //animator.SetBool("isShooting", true);
-
-        if (Time.time >= nextTimeToFire)
-        {
-            nextTimeToFire = Time.time + 1f / fireRate;
-            Fire();
-        }
-    }
-
-    private void Update()
-    {
         if (isReloading)
             return;
 
         bool isShooting = shoot.ReadValue<float>() > 0.5f;
-        //animator.SetBool("isShooting", isShooting);
+
+        if (isShooting && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Fire();
+        }
 
         if (currentAmmo == 0 && magazineAmmo > 0 && !isReloading)
         {
@@ -87,6 +67,7 @@ public class Gun : MonoBehaviour
     private void Fire()
     {
         AudioManager.instance.Play("Shoot");
+
         muzzleFlash.Play();
         currentAmmo--;
         RaycastHit hit;
@@ -111,12 +92,11 @@ public class Gun : MonoBehaviour
         }
     }
 
-    private IEnumerator Reload()
+    IEnumerator Reload()
     {
         isReloading = true;
-        //animator.SetBool("isReloading", true);
+        AudioManager.instance.Play("Reload");
         yield return new WaitForSeconds(reloadTime);
-        //animator.SetBool("isReloading", false);
         if (magazineAmmo >= maxAmmo)
         {
             currentAmmo = maxAmmo;
